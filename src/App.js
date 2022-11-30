a3cb5622cc0224160153a4c24af548ebaa6fc5d5
Samu_l <69546753+tetr-git@users.noreply.github.com>
2022-11-30 23:41:36 +0100

    added SearchBarLogic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
+import * as React from "react";
 import { useState, useEffect } from "react";
+import TextField from "@mui/material/TextField";
+
+/*
+@sources
+https://dev.to/salehmubashar/search-bar-in-react-js-545l
+
+*/
 
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
+  const [inputText, setInputText] = useState("");
+  const inputRef = React.useRef(null);
 
-  // Note: the empty deps array [] means
-  // this useEffect will run once
-  // similar to componentDidMount()
   useEffect(() => {
     fetch("https://swapi.dev/api/films/")
       .then((res) => res.json())
@@ -17,9 +24,6 @@ function App() {
           setIsLoaded(true);
           setItems(result);
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
         (error) => {
           setIsLoaded(true);
           setError(error);
@@ -27,18 +31,48 @@ function App() {
       );
   }, []);
 
+  let inputHandler = (e) => {
+    var lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
     return <div>Loading...</div>;
   } else {
+    const filteredData = items.results.filter((el) => {
+      if (inputText === "") {
+        return el;
+      } else {
+        return el.title.toLowerCase().includes(inputText);
+      }
+    });
     //console.log(JSON.stringify(items, null, 2));
     return (
-      <ul>
-        {items.results.map((results) => (
-          <li key={results.title}>{results.title}</li>
-        ))}
-      </ul>
+      <div className="main">
+        <h1>Star Wars Api Internal 2</h1>
+        <div className="search">
+          <TextField
+            id="outlined-basic"
+            onChange={inputHandler}
+            variant="outlined"
+            fullWidth
+            label="Search"
+          />
+        </div>
+        <ul>
+          {filteredData.map((item) => (
+            <li
+              ref={inputRef}
+              onClick={() => console.log("clicked")}
+              key={item.title}
+            >
+              {item.title}
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
